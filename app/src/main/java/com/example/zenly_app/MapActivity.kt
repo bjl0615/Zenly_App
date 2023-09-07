ceecebe93e611c2a796303d4dba8641ed05e85d7
admin <bjl0615@gmail.com>
2023-09-07 16:50:56 +0900

    Zenly_App Other User Reaction Send Part_1 Add

--- app/src/main/java/com/example/zenly_app/MapActivity.kt
+++ app/src/main/java/com/example/zenly_app/MapActivity.kt
@@ -3,12 +3,20 @@ package com.example.zenly_app
 import android.Manifest
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Looper
 import android.util.Log
+import android.view.RoundedCorner
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.zenly_app.databinding.ActivityMapBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
@@ -18,8 +26,12 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
@@ -31,22 +43,26 @@ import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import com.kakao.sdk.common.util.Utility
 
-class MapActivity : AppCompatActivity() , OnMapReadyCallback {
-    private lateinit var binding : ActivityMapBinding
+class MapActivity : AppCompatActivity(), OnMapReadyCallback , GoogleMap.OnMarkerClickListener {
+    private lateinit var binding: ActivityMapBinding
     private lateinit var googleMap: GoogleMap
-    private lateinit var fusedLocationClient : FusedLocationProviderClient
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    private val markerMap = hashMapOf<String , Marker>()
+    private var trackingPersonId : String = ""
+    private val markerMap = hashMapOf<String, Marker>()
+
     private val locationPermissionRequest = registerForActivityResult(
         ActivityResultContracts.RequestMultiplePermissions()
-    ) {permissions ->
+    ) { permissions ->
         when {
-            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION , false) -> {
+            permissions.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
                 getCurrentLocation()
             }
+
             permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
                 getCurrentLocation()
             }
+
             else -> {
 
             }
@@ -56,22 +72,26 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
     private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(locationResult: LocationResult) {
             // 새로 요청 된 위치 정보
-            for(location in locationResult.locations) {
+            for (location in locationResult.locations) {
 
-                Log.e("MapActivity" , "onLocationResult : ${location.latitude} , ${location.longitude}")
+                Log.e(
+                    "MapActivity",
+                    "onLocationResult : ${location.latitude} , ${location.longitude}"
+                )
 
                 val uid = Firebase.auth.currentUser?.uid.orEmpty()
-                if(uid == "") {
+                if (uid == "") {
                     finish()
                 }
 
-                val locationMap = mutableMapOf<String , Any>()
+                val locationMap = mutableMapOf<String, Any>()
                 locationMap["latitude"] = location.latitude
                 locationMap["longitude"] = location.longitude
                 Firebase.database.reference.child("Person").child(uid).updateChildren(locationMap)
             }
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
@@ -116,11 +136,15 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
             return
         }
         // 권한이 있는 상태
-        fusedLocationClient.requestLocationUpdates(locationRequest , locationCallback , Looper.getMainLooper())
+        fusedLocationClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
 
         fusedLocationClient.lastLocation.addOnSuccessListener {
             googleMap.animateCamera(
-                CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude , it.longitude), 16.0f)
+                CameraUpdateFactory.newLatLngZoom(LatLng(it.latitude, it.longitude), 16.0f)
             )
         }
     }
@@ -140,7 +164,7 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
                 override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
                     val person = snapshot.getValue(Person::class.java) ?: return
                     val uid = person.uid ?: return
-                    if(markerMap[uid] == null) {
+                    if (markerMap[uid] == null) {
                         markerMap[uid] = makeNewMarker(person, uid) ?: return
                     }
                 }
@@ -150,10 +174,22 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
                     val uid = person.uid ?: return
 
 
-                    if(markerMap[uid] == null) {
-                        markerMap[uid] = makeNewMarker(person , uid) ?: return
+                    if (markerMap[uid] == null) {
+                        markerMap[uid] = makeNewMarker(person, uid) ?: return
                     } else {
-                        markerMap[uid]?.position = LatLng(person.latitude ?: 0.0 , person.longitude ?: 0.0)
+                        markerMap[uid]?.position =
+                            LatLng(person.latitude ?: 0.0, person.longitude ?: 0.0)
+                    }
+
+                    if(uid == trackingPersonId) {
+                        googleMap.animateCamera(
+                            CameraUpdateFactory.newCameraPosition(
+                                CameraPosition.Builder()
+                                    .target(LatLng(person.latitude ?: 0.0, person.longitude ?: 0.0))
+                                    .zoom(16.0f)
+                                    .build()
+                            )
+                        )
                     }
                 }
 
@@ -168,14 +204,50 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
             })
     }
 
-    private fun makeNewMarker(person : Person , uid : String) : Marker?{
+    private fun makeNewMarker(person: Person, uid: String): Marker? {
         val marker = googleMap.addMarker(
             MarkerOptions()
                 .position(LatLng(person.latitude ?: 0.0, person.longitude ?: 0.0))
                 .title(person.name.orEmpty())
         ) ?: return null
 
+        marker.tag = uid
 
+        Glide.with(this).asBitmap()
+            .load(person.profilePhoto)
+            .override(200)
+            .transform(RoundedCorners(60))
+            .listener(object : RequestListener<Bitmap> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+
+                override fun onResourceReady(
+                    resource: Bitmap?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+
+                    resource?.let {
+                        runOnUiThread {
+                            marker.setIcon(
+                                BitmapDescriptorFactory.fromBitmap(
+                                    resource
+                                )
+                            )
+                        }
+                    }
+                    return true
+                }
+
+            }).submit()
         return marker
     }
 
@@ -185,5 +257,17 @@ class MapActivity : AppCompatActivity() , OnMapReadyCallback {
         googleMap.setMaxZoomPreference(20.0f)
         googleMap.setMinZoomPreference(10.0f)
 
+        googleMap.setOnMarkerClickListener(this)
+        googleMap.setOnMapClickListener {
+            trackingPersonId = ""
+        }
+    }
+
+    override fun onMarkerClick(marker: Marker): Boolean {
+
+        trackingPersonId = marker.tag as? String ?: ""
+
+        return false
     }
 }
+
